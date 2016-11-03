acbb50ae7c7cdba8fd29f4ef50f42c0f1a6f4d1d
Akram <akramewu@gmail.com>
2016-11-03 22:54:14 +0100

    UnitTestforAddingTwoNumbersCheck

---
+++ Calculator.Tests/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.Add(3, 5);
+
+            Assert.That(result,Is.EqualTo(8));
+        }
+    }
+}
---
+++ Calculator/SimpleCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
